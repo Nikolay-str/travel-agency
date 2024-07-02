4fa6ac2a408c3da8077bc655e5808961ed51c225
Nikolay-str <55555mett@gmail.com>
2024-07-02 18:56:27 +0300

    lhglhj

---
+++ Part_1/block 1/task_3.sql
@@ -0,0 +1,43 @@
+CREATE TABLE tourist
+(
+    id_tourist SERIAL,
+    firstname TEXT NOT NULL,
+    middlename TEXT NOT NULL,
+    lastname TEXT,
+    phone INTEGER NOT NULL,
+    tour INTEGER,
+
+    PRIMARY KEY(id_tourist)
+    FOREIGN KEY(tour)
+        REFERENCES tour(id_tour)
+
+);
+
+CREATE TABLE tour
+(
+    id_tour SERIAL,
+    tour_where TEXT,
+    tour_from_where TEXT,
+    hotel INTEGER,
+    days INTEGER,
+
+    PRIMARY KEY(id tour)
+    FOREIGN KEY(hotel)
+
+);
+
+CREATE TABLE hotel
+(
+    id_hotel SERIAL,
+    stars INTEGER,
+    room_standart : INTEGER
+    room_delux : INTEGER
+
+
+    PRIMARY KEY(id_hotel)
+    FOREIGN KEY(id_hotel)
+        REFERENCES thor(hotel)
+
+);
+
+
